3a0b25e5880cf4b09af733fdc69ae6cf029fd319
--replace-all <harperal@oregonstate.edu>
2023-08-07 18:17:38 -0400

    working server with email confirmation to contactus.html page

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');  // Import the dotenv package
+
+// Load environment variables from .env file
+dotenv.config();
+
 const app = express();
-const port = 5505; 
+const port = process.env.PORT || 5505; 
 
-app.use(cors());
-
+app.use(cors({ origin: '*' })); // Allow requests from any origin during development
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname));
 
 
 // Handle form submission
-app.post('/submit_contact_form', async (req, res) => {
+app.post('/contactForm', async (req, res) => {
   // Get form data
   const firstName = req.body['form-first-name'];
   const lastName = req.body['form-last-name'];
@@ -26,31 +31,38 @@ app.post('/submit_contact_form', async (req, res) => {
     await sendEmail(email, subject, body);
 
     // Send a response back to the client
-    res.send('Thank you for contacting us! A confirmation email has been sent to your email address.');
+    //res.send('Thank you for contacting us! A confirmation email has been sent to your email address.');
+    res.redirect('/confirmation/contact.html');
   } catch (error) {
     console.error('Error sending email:', error);
     res.status(500).send('An error occurred while sending the email.');
   }
 });
 
-
 // Function to send an email
 async function sendEmail(to, subject, text) {
-  // Create a testing account transporter
+  // Create a transporter using GMAIL SMTP, transporter below is for testing purposes only
+  //const transporter = nodemailer.createTransport({
+    //service: 'Gmail',  
+    //auth: {
+      //user: process.env.EMAIL_USER,  // Use environment variable
+      //pass: process.env.EMAIL_PASS,  // Use environment variable
+    //},
+  //});
   const transporter = nodemailer.createTransport({
     host: 'smtp.ethereal.email',
     port: 587,
     auth: {
-      user: 'otho42@ethereal.email', // Generated test email username
-      pass: 'z7dRzSRHgVCRPFkkJy' // Generated test email password
+        user: 'dorian.mcclure40@ethereal.email',
+        pass: 'tjfA6vwm8hkvb5XPb3'
     }
-  });
+});
 
   const mailOptions = {
-    from: 'harper.2480@gmail.com', 
+    from: process.env.EMAIL_USER, 
     to: to,
     subject: subject,
-    text: text
+    text: text,
   };
 
   // Send the email
